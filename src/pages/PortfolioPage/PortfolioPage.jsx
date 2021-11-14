3d4604b6b35a6066f9d575f43f4d35e50ee54cae
OshriYoktan <oshriyok1@gmail.com>
2021-11-14 17:43:36 +0200

    first commit

--- src/pages/PortfolioPage/PortfolioPage.jsx
+++ src/pages/PortfolioPage/PortfolioPage.jsx
@@ -13,15 +13,15 @@ export const PortfolioPage = () => {
                     <p>Project Management Tool</p>
                 </div>
                 </a>
-                <a className="portfolio-project proj-img jukify" target="_blank" rel="noreferrer" href="http://taskman-app.herokuapp.com/#/"><div>
+                <a className="portfolio-project proj-img jukify" target="_blank" rel="noreferrer" href="http://jukify-music-app.herokuapp.com/"><div>
                     <h4>Jukify</h4>
-                    <p>Project Management</p>
+                    <p>Song Player Based On Spotify And Jukebox</p>
                 </div> </a>
-                <a className="portfolio-project proj-img appsus" target="_blank" rel="noreferrer" href="http://taskman-app.herokuapp.com/#/"><div>
+                <a className="portfolio-project proj-img appsus" target="_blank" rel="noreferrer" href="https://oshriyoktan.github.io/Appsus/#/"><div>
                     <h4>Appsus</h4>
                     <p>Notes And Mailbox</p>
                 </div></a>
-                <a className="portfolio-project proj-img mister-bit" target="_blank" rel="noreferrer" href="http://taskman-app.herokuapp.com/#/"><div >
+                <a className="portfolio-project proj-img mister-bit" target="_blank" rel="noreferrer" href="https://mister-toys-app.herokuapp.com/#/"><div >
                     <h4>Mister Toy</h4>
                     <p>Toys Shopping Website</p>
                 </div></a>
